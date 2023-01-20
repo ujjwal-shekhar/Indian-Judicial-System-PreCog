992ef510a7038c045d5d3b4226157bf17edad2eb
ujjwal-shekhar <95298765+ujjwal-shekhar@users.noreply.github.com>
2023-01-20 21:49:38 +0530

    Delete master_csv_control.py

--- master_csv_control.py
+++
@@ -1,7 +0,0 @@
-import os
-
-JUDICIAL_DATA_CSV_FOLDER_PATH = "/mnt/e/judicial_data_csv/"
-ACTS_SECTIONS_CSV = os.path.join(JUDICIAL_DATA_CSV_FOLDER_PATH, "acts_sections.csv")
-CASES_CSV_FOLDER = os.path.join(JUDICIAL_DATA_CSV_FOLDER_PATH, "cases/cases/")
-JUDGES_CSV = os.path.join(JUDICIAL_DATA_CSV_FOLDER_PATH, "judges_clean.csv")
-KEYS_CSV = os.path.join(JUDICIAL_DATA_CSV_FOLDER_PATH, "keys.csv")
